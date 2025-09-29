8a8044c5268753d63bf001ef4047707a393db492
M Nas Solutions <155361855+MNasSolutions@users.noreply.github.com>
2025-09-29 15:59:49 +0100

    Update FeaturedProducts.tsx

--- src/components/sections/FeaturedProducts.tsx
+++ src/components/sections/FeaturedProducts.tsx
@@ -2,7 +2,7 @@ import { Star, Heart, ShoppingCart } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { Link } from "react-router-dom";
-import mnas from "assets/2.jpg";
+import {mnas} from "assets/2.jpg";
 
 // Mock product data
 const products = [
