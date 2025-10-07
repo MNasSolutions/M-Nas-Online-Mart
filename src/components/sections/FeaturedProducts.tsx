4c4e1e4132fe987624605c8937d420fbe0ecff92
M Nas Solutions <155361855+MNasSolutions@users.noreply.github.com>
2025-10-07 13:26:32 +0100

    Update FeaturedProducts.tsx

--- src/components/sections/FeaturedProducts.tsx
+++ src/components/sections/FeaturedProducts.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button";
 import { useToast } from "@/hooks/use-toast";
 import { Link } from "react-router-dom";
 import productImage from "@/assets/data/2.jpg";
+
 // Mock product data
 const products = [
   {
