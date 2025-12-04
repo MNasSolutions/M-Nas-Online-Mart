f18db5ed617b0928680324c2d6809accab8788f5
M Nas Solutions <155361855+MNasSolutions@users.noreply.github.com>
2025-12-04 16:16:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,16 +18,3 @@ export default defineConfig(({ mode }) => ({
 }));
 
 
-// vite.config.ts
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-});
